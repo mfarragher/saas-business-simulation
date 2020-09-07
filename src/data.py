76f536d3932a4105952ca7a3443c731ca712604c
mfarragher <33595938+mfarragher@users.noreply.github.com>
2020-09-07 22:35:14 +0100

    Mechanics for generating user activity data

--- src/data.py
+++ src/data.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 from scipy.stats import skewnorm
 
-from src._user_growth import get_user_counts_by_date
+from src._user_growth import get_user_counts_by_date, get_active_user_counts_by_date
 
 
 def get_user_dataset(start_date, end_date, *,
@@ -105,3 +105,104 @@ def _fill_activation_dates(df, *, new_users_by_date, start_users, seed=None):
     date_values = (pd.concat([legacy_dates, new_dates],
                              ignore_index=True).values)
     return df.assign(activation_date=lambda x: date_values)
+
+
+def get_user_activity_dataset(start_date, end_date, *, users_df,
+                              approx_yoy_growth_rate=3, start_users=10000,
+                              seed=None):
+    active_user_counts = get_active_user_counts_by_date(
+        start_date, end_date,
+        approx_yoy_growth_rate=approx_yoy_growth_rate,
+        start_users=start_users,
+        seed=seed)
+
+    # define initial col order
+    columns = ['session_id', 'user_id', 'session_start_date']
+
+    activity_df = pd.DataFrame(columns=columns)
+    activity_df['session_start_date'] = pd.Series((active_user_counts.dropna()
+                                                   .repeat(active_user_counts.dropna()))
+                                                  .index.values)
+    activity_df['session_id'] = _get_uuid_values(len(activity_df))  # not seeded
+    # choose active user IDs via a sampling function
+    activity_df = (activity_df
+                   .pipe(_sample_user_ids,
+                         users_df=users_df,
+                         active_user_counts=active_user_counts,
+                         start_date=start_date,
+                         end_date=end_date,
+                         seed=seed)
+                   .drop(columns=['user_activation_date'])  # drop joined col
+                   )
+
+    return activity_df.set_index('session_id')
+
+
+def _sample_user_ids(activity_df, *, users_df, active_user_counts,
+                     start_date, end_date, seed=None):
+    df = activity_df.copy()
+
+    # on avg, uuid digits sum near 100 and give norm dist
+    sum_of_user_id_digits = [sum(i)
+                             for i in [[int(i) for i in ''.join(s)]
+                                       for s in (users_df.index
+                                                 .str.findall("(\d*\.?\d+)"))]]
+    # make stickiness score from UUID (positive skew)
+    stickiness_score = (pd.Series(sum_of_user_id_digits,
+                                  index=users_df.index) ** 2 // 100)
+
+    for d in pd.date_range(start_date, end_date, closed='left'):
+        list_active_users_to_add = []
+
+        dau = active_user_counts.loc[d]
+
+        filtered_df = users_df[users_df['activation_date'] <= d].copy()
+        filtered_df['days_since_activation'] = (d - filtered_df['activation_date']).dt.days
+
+        new_users = np.unique(filtered_df[filtered_df['days_since_activation'] == 0]
+                              .index.tolist())
+
+        # make all new users active for this day
+        list_active_users_to_add.extend(new_users)
+
+        # for remaining active users to fill, use a sampling function
+        active_users_left_to_add = dau - len(new_users)
+
+        active_existing_user_ids = _draw_existing_users_to_set_as_active(
+            filtered_df,
+            stickiness_score=stickiness_score,
+            n_draws=active_users_left_to_add
+        )
+
+        list_active_users_to_add.extend(active_existing_user_ids)
+
+        # set user ids
+        df.loc[df['session_start_date'] == d,
+               'user_id'] = list_active_users_to_add
+
+    return (df
+            .merge(users_df['activation_date'].rename('user_activation_date'),
+                   how='left', left_on=['user_id'], right_index=True))
+
+
+def _draw_existing_users_to_set_as_active(existing_users_df, *, stickiness_score, n_draws):
+    filtered_df = existing_users_df[existing_users_df['days_since_activation'] > 0].copy()
+    existing_users = np.unique(filtered_df.index.tolist())
+
+    # get weights for users who were created before date (inclusive) (sum to 1)
+    stickiness_weights = stickiness_score.filter(existing_users, axis=0)
+
+    # decay function (horizontal asymptote to encourage an engagement floor)
+    filtered_df['stickiness_decay_factor'] = 0.8 * 0.9 ** filtered_df['days_since_activation'] + 0.1
+
+    # weighted function (of sticky user IDs and of time since activation )
+    stickiness_weights = 0.2 * stickiness_weights + 0.8 * (40 * filtered_df['stickiness_decay_factor'])
+    # sum weights to 1 before doing the sampling
+    stickiness_weights = stickiness_weights / stickiness_weights.sum()
+
+    # sample active users via stickiness weights (sample -> no replacement)
+    active_existing_user_ids = np.random.choice(stickiness_weights.index.tolist(),
+                                                size=n_draws,
+                                                replace=False,
+                                                p=stickiness_weights.tolist())
+    return active_existing_user_ids
